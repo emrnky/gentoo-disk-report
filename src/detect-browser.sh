fe498ae5e85e20f575fcb125f341ca42d42d02f1
emrnky <emirhan.kayar80@gmail.com>
2026-09-19 21:49:53 +0200

    Get total cache bytes directly from json, for summary print of the section. Now tested with firefox-bin installation also it works.

--- src/detect-browser.sh
+++ src/detect-browser.sh
@@ -2,6 +2,7 @@
 # git clone git@github.com:emrnky/browser_cleanup_tools.git 
 # cd browser_cleanup_tools
 # chmod +x *.sh lib/*.sh 
+source "src/common.sh"
 
 browserTools="browser_cleanup_tools"
 json=$("$browserTools/disk-report.sh" --json)
@@ -9,13 +10,11 @@ json=$("$browserTools/disk-report.sh" --json)
 mapfile -t browserNames  < <(grep -oP '"app": "\K[^"]+' <<< "$json")
 mapfile -t cacheBytes    < <(grep -oP '"cache_bytes": \K[0-9]+' <<< "$json")
 
+browserCacheTotal=$(grep -oP '"total_cache_bytes": \K[0-9]+' <<< "$json")
+
 scripts=()
 sizes=()
 for i in "${!browserNames[@]}"; do
     scripts+=("clean-$(awk '{print tolower($1)}' <<< "${browserNames[i]}").sh")
-    sizes+=("$(numfmt --to=iec --format='%.1f' "${cacheBytes[i]}")")
+    sizes+=("$(bytesToHooman "${cacheBytes[i]}")")
 done
-
-# for i in "${!browserNames[@]}"; do
-#     printf '%-20s %-20s %s\n' "${scripts[i]}" "${browserNames[i]}" "${sizes[i]}"
-# done
