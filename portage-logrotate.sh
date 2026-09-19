058efc98c2315af401d28b9882e28bc4a5be3860
emrnky <emirhan.kayar80@gmail.com>
2026-09-19 21:47:36 +0200

    Add portage log directories config from wiki page to logrotate.d directory automatically.

--- portage-logrotate.sh
+++ portage-logrotate.sh
@@ -1,3 +1,13 @@
+# Documentation : https://wiki.gentoo.org/wiki/Logrotate
+# This script adds the following string
+# into the below directory so that,
+# logrotate can work on portage files
+# /etc/logrotate.d/portage
+path="/etc/logrotate.d/"
+
+if [[ -d "$path" ]]; then
+    touch "$path/portage"
+    sudo tee "$path/portage" > /dev/null <<'EOF'
 # /etc/logrotate.d/portage
 
 /var/log/emerge-fetch.log {
@@ -21,3 +31,9 @@
     missingok
     nocreate
 }
+EOF
+else
+    echo "Logrotate path does not exist./Logrotate is not installed."
+    echo "In order to install run:"
+    echo "sudo emerge --ask app-admin/logrotate"
+fi
